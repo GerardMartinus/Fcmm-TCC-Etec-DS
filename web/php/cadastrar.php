e9d06402c4a4a28e3e14e3961a648636283bea5f
Bieldeid <gabrieldeid.android@gmail.com>
2022-04-07 20:44:54 -0300

    inicando o back end (cadastro e login)

--- web/php/cadastrar.php
+++ web/php/cadastrar.php
@@ -2,6 +2,28 @@
 session_start();
 include("cadastro.html");
 
-$nome = mysqli_real_escape_string($conexao, $_POST['apelido'])
+$apelido = mysqli_real_escape_string($conexao, $_POST['apelido']);
+$email = mysqli_real_escape_string($conexao, $_POST['email']);
+$senha = mysqli_real_escape_string($conexao, $_POST['senha']);
 
+$sql = "select count(*) as total from usuario where usuario = '$usuario'";
+$result = mysqli_query($sql);
+$row = mysqli_fetch_assoc($result);
+
+if($row['total'] == 1) {
+    $_SESSION['usuario_existe'] = true;
+    header('Location: cadastro.php');
+    exit;
+}
+
+$sql = "INSERT INTO usuario (apelido, email, senha) VALUES ('$apelido', '$email', '$senha', NOW())";
+
+if($conexao->query($sql) === TRUE) {
+    $_SESSION['status_cadastro'] = true;
+}
+
+$conexao->close();
+
+header('Location: cadastro.php');
+exit;
 ?>
