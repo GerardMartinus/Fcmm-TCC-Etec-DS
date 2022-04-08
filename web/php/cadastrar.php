934dded4729b46d4a11738cc73d303ce2d0ffc88
Bieldeid <gabrieldeid.android@gmail.com>
2022-04-07 22:11:03 -0300

    O BANCO DE DADOS TA FUNCIONANDOOOOO UFEEEEEEEEE 😊😎❤

--- web/php/cadastrar.php
+++ web/php/cadastrar.php
@@ -6,17 +6,17 @@ $apelido = mysqli_real_escape_string($conexao, $_POST['apelido']);
 $email = mysqli_real_escape_string($conexao, $_POST['email']);
 $senha = mysqli_real_escape_string($conexao, $_POST['senha']);
 
-$sql = "select count(*) as total from usuario where usuario = '$usuario'";
-$result = mysqli_query($sql);
+$sql = "select count(*) as total from cadastro where apelido = '$apelido'";
+$result = mysqli_query($conexao, $sql);
 $row = mysqli_fetch_assoc($result);
 
 if($row['total'] == 1) {
     $_SESSION['usuario_existe'] = true;
-    header('Location: cadastro.php');
+    header('Location: ../html/cadastroexiste.html');
     exit;
 }
 
-$sql = "INSERT INTO usuario (apelido, email, senha) VALUES ('$apelido', '$email', '$senha', NOW())";
+$sql = "INSERT INTO cadastro (apelido, email, senha) VALUES ('$apelido', '$email', '$senha')";
 
 if($conexao->query($sql) === TRUE) {
     $_SESSION['status_cadastro'] = true;
@@ -24,6 +24,6 @@ if($conexao->query($sql) === TRUE) {
 
 $conexao->close();
 
-header('Location: cadastro.php');
+header('Location: ../html/cadastro.html');
 exit;
 ?>
