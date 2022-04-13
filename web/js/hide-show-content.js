b5e845bf28566058bd0588f695285fb8927cf721
GerardMartinus <gerardmartinussilva@gmail.com>
2022-04-13 16:20:39 -0300

    index, css reformulado e btn esconder

---
+++ web/js/hide-show-content.js
@@ -0,0 +1,26 @@
+function hideIntro(){
+    var intro = document.getElementById("introducao");
+    if (intro.style.display === "none"){
+        intro.style.display = "block";
+    } else {
+        intro.style.display = "none";
+    }
+}
+
+function hideSobre(){
+    var intro = document.getElementById("sobre");
+    if (intro.style.display === "none"){
+        intro.style.display = "block";
+    } else {
+        intro.style.display = "none";
+    }
+}
+
+function hideConteudo(){
+    var intro = document.getElementById("conteudo");
+    if (intro.style.display === "none"){
+        intro.style.display = "block";
+    } else {
+        intro.style.display = "none";
+    }
+}
