4a8743e453efbdb013f22aea631eff7c417e29a2
Bieldeid <gabrieldeid.android@gmail.com>
2022-05-30 19:50:07 -0300

    adicionado o VLibras nas páginas

--- web/php/cadastro.php
+++ web/php/cadastro.php
@@ -58,5 +58,16 @@
             <a href="login.php" id="login">Logar</a>
         </div>
     </div>
+      <!-- Adicionando o VLibras -->  
+  <div vw class="enabled">
+    <div vw-access-button class="active"></div>
+    <div vw-plugin-wrapper>
+      <div class="vw-plugin-top-wrapper"></div>
+    </div>
+  </div>
+  <script src="https://vlibras.gov.br/app/vlibras-plugin.js"></script>
+  <script>
+    new window.VLibras.Widget('https://vlibras.gov.br/app');
+  </script>
 </body>
 </html>
