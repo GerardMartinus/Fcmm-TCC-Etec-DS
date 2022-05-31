55a71980b45f132cec3ebe921339edaaedb29d02
Bieldeid <gabrieldeid.android@gmail.com>
2022-05-30 22:32:10 -0300

    Esqueceu senha não funcionando ainda, amanhã termino

--- web/php/esqueceusenha.php
+++ web/php/esqueceusenha.php
@@ -1,21 +1,22 @@
 <?php
     include("config.php");
 
-    if(isset($_POST[ok])){
+    if(isset($_POST["ok"])){
+      $email = $_POST['email'];
 
-        $email = $mysqli>-escape_string($_POST['email']);
-
-        if(!filter_var($email, FILTER_VALIDATE_EMAIL)){
+        if(!filter_var($email, FILTER_VALIDATE_EMAIL))
             $erro[] = "E-mail inválido";
-        }
+        
 
-        $sql_code = "SELECT senha, nome FROM usuario WHERE email = '$_SESSION[email]'";
+        $sql_code = "SELECT * FROM usuario WHERE email = '$email'";
         $sql_query = $mysqli->query($sql_code) or die ($mysqli->error);
         $dado = $sql_query->fetch_assosc(); 
         $total = $sql_query->num_rows;
 
-        if($total == 0)
-
+        if($total == 0){
+          $erro[] = "O e-mail informado não existe no banco de dados.";
+        }
+            
         if(count($erro) == 0 && total > 0){
             $novasenha = substr(md5(time()),0 ,6);
             $nscriptografada = md5(md5($novasenha));
@@ -23,6 +24,12 @@
             if(mail($email, "Sua nova senha", "Sua nova senha: ".$novasenha)){
                 $sql_code = "UPDATE usuario SET senha = '$nscriptografada' WHERE email = '$email'";
                 $sql_query = $mysqli->$query($sql_code) or die ($mysqli->error);
+
+                if($sql_query)
+                  $erro[] = "Senha alterada com sucesso!";
+
+                else
+                $erro[]="Sua senha não foi alterada!";
             }
         }
 
@@ -35,13 +42,17 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link rel="stylesheet" href="../css/cadastro.css">
+    <title>Esqueci minha senha</title>
 </head>
 <body>
-    <form action="">
-        <input type="email" placeholder="Digite seu email" name="email">
+  <div id="login-container">
+    <h2>Digite seu email para a alteração da senha</h2>
+    <form action="esqueceusenha.php" method="POST">
+        <input value="<?php echo $_POST['$email']; ?>"  type="email" placeholder="Digite seu email" name="email">
         <input type="submit" value="Enviar" name="ok">
     </form>
+  </div>
 
       <!-- Adicionando o VLibras -->  
   <div vw class="enabled">
