e4811a4d89ddcfc43a76828e17bc9ab95e6373b4
Bieldeid <gabrieldeid.android@gmail.com>
2022-06-07 22:32:56 -0300

    Validação do cadsatro em JS FUNCIONANDO AEEEEEEEE

--- web/js/validar.js
+++ web/js/validar.js
@@ -1,61 +1,70 @@
+function validarCampos(){
 
-let nome = cadastro.nome.value;
-let email = cadastro.email.value;
-let senha = cadastro.senha.value;
-let confirmar = cadastro.confirmar.value;
+    let nome = document.getElementById('nome');
+    let email = document.getElementById('email');
+    let senha = document.getElementById('senha');
+    let confirmar = document.getElementById('confirmar');
 
-
-function validar(){
-    if(nome == ""){
-        alert ("Campo 'Nome' vazio!");
-        cadastro.nome.focus();
+    if (nome.value == ""){
+        Swal.fire({
+            icon:'error',
+            title: 'Campo nome vazio',
+            text: 'Por favor, preencha o campo "nome" corretamente.'
+        })
+        nome.focus();
         return false;
     }
 
-    if (email == "") {
-        alert ("Campo 'Email' vazio!");
-        cadastro.email.focus();
+    if (email.value == ""){
+        Swal.fire({
+            icon: 'error',
+            title: 'Campo email vazio',
+            text: 'Por favor, preencha o campo "e-mail" corretamente.'
+        })
+        email.focus();
         return false;
     }
 
-    if (senha == "") {
-        alert ("Campo 'senha' vazio!");
-        cadastro.senha.focus();
+    if (confirmar.value != senha.value){
+        Swal.fire({
+            icon: 'error',
+            title: 'Senhas não confirmam',
+            text: 'As senhas estão diferentes, favor preencher corretamente' 
+        })
+        confirmar.focus();
         return false;
     }
 
-    if (confirmar == "") {
-        alert ("Campo 'Confirmar Senha' vazio!");
-        cadastro.confirmar.focus();
-        return false;
+    let regex = /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[^\w\s]).{6,16}$/
+    if (!regex.exec(senha.value)){
+        Swal.fire({
+            icon: 'error',
+            title: 'Senha inválida',
+            text: 'A senha deve conter entre 6 a 16 caracteres sendo pelo menos 1 letra maiúsula e 1 minúscula, 1 numero e 1 caractere especial.'
+        })
+        senha.focus()
+        return false
     }
 
-    if(isNaN(nome)){
-        alert ("Campo 'nome' contém números!");
-        cadastro.cpf.focus();
-        return false;
+    let timerInterval
+    Swal.fire({
+        color: '#04631d',
+        title: 'Cadastro realizado com sucesso!',
+        timer: 2000,
+        didOpen: () => {
+            Swal.showLoading()
+            const b = Swal.getHtmlContainer().querySelector('b')
+            timerInterval = setInterval(() => {
+            b.textContent = Swal.getTimerLeft()
+            }, 100)
+        },
+        willClose: () => {
+            clearInterval(timerInterval)
+        }
+    }).then((result) => {
+    /* Read more about handling dismissals below */
+    if (result.dismiss === Swal.DismissReason.timer) {
+        console.log('I was closed by the timer')
     }
-    
-    if(senha != confirmar){
-        alert ("Senhas estão diferentes");
-        cadastro.confirmar.focus();
-        return false;
-    }
-
-    let regex = /^(?=.*?[A-Za-z0-9_])(?=. *?[^\w\s]).{6,20}$/
-    /*
-    (?=.*?[A-Z]) - minimo uma letra maiscula.
-    (?=.*?[a-z]) - minimo uma letra minuscula.
-    (?=.*?[0-9]) - minimo de número.
-    (?=.*?[^\w\s]) - minimo de caractere especial.
-    .{6.10} - minimo 6, maximo 10 caracteres.
-     */
-
-    if(!regex.exec(senha)){
-        alert ("A senha deve conter: uma letra maíuscula, minúscula, um número e um caractere especial");
-        cadastro.senha.focus();
-        return false;
-    }
-
+    })
 }
-
