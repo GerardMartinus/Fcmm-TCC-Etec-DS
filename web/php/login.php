b46d390a2b1fe7bab8a6e894148a6e3f20bd81bf
Bieldeid <gabrieldeid.android@gmail.com>
2022-06-10 19:46:18 -0300

    Alterei algumas coisas, adicionei navbar no login mas deu errado, os pop-up estão funcionando!

--- web/php/login.php
+++ web/php/login.php
@@ -30,6 +30,7 @@ if (isset($_POST['submit'])) {
   <meta name="author" content="Bruno Martins de Morais Silva, Arthur Souto, Gabriel Santos, Ana Luisa, Igor">
   <meta name="keywords" content="">
   <link rel="stylesheet" href="../css/login.css">
+  <link rel="stylesheet" href="../css/index.css" />
   <link rel="stylesheet" href="http://fonts.googleapis.com">
   <link rel="stylesheet" href="http://fonts.gstatic.com" crossorigin>
   <link rel="stylesheet" href="http://fonts.googleapis.com/css2?family=Roboto=wght@100;300;400;500;700&display=swap">
@@ -37,11 +38,37 @@ if (isset($_POST['submit'])) {
 </head>
 
 <body>
+  <header>
+  <div class="acessibilidade">
+      <div class="btn-container">
+        <label class="label-1">Contraste:</label>
+        <button name="increase-font" id="increase-font" title="Aumentar fonte">A +</button>
+        <button name="decrease-font" id="decrease-font" title="Diminuir fonte">A -</button>
+      </div>
+    </div>
+
+    <!-- navbar -->
+    <nav id="nav-header">
+      <a id="logo" href="index.html">Fale com Minha Mão</a>
+      <div class="mobile-menu">
+        <div class="line1"></div>
+        <div class="line2"></div>
+        <div class="line3"></div>
+      </div>
+      <ul class="nav-list">
+        <li><a href="index.html">Inicio</a></li>
+        <li><a href="unidade1.html">Sobre nós</a></li>
+        <li><a href="unidade2.html">Fale conosco</a></li>
+        <li><a href="../php/login.php">Login</a></li>
+        <li><a href="../php/cadastro.php">Cadastro</a></li>
+      </ul>
+    </nav>
+  </header>
 
 
   <div id="login-container">
     <h1>Bem-vindo de volta!</h1>
-    <form action="">
+    <form name="cadastro" id="cadastro" method="POST" onsubmit="return validarCampos()">
       <input type="email" name="email" id="email" placeholder="E-mail">
       <input type="password" name="senha" id="senha" placeholder="Senha">
       <input type="submit" value="Entrar">
@@ -73,6 +100,12 @@ if (isset($_POST['submit'])) {
   <script>
     new window.VLibras.Widget('https://vlibras.gov.br/app');
   </script>
+
+<script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+  <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+  <script src="../js/fonte.js"></script>
+  <script src="../js/contraste-views.js"></script>
+  <script src="../js/validarLogin.js"></script>
 </body>
 
 </html>
